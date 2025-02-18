1dc5228a47d69ff8879951f47a9ec38f00eae748
Oleksandr Bondar <aleksadnrr.bondar@gmail.com>
2025-02-18 22:11:51 +0200

    Added vulkan API

--- GameExample/main.cpp
+++ GameExample/main.cpp
@@ -1,3 +1,5 @@
+#define GLFW_INCLUDE_VULKAN
+
 #include "Window/Window.h"
 
 #include "GLWrapper/ShaderProgram/ShaderProgram.h"
@@ -7,10 +9,29 @@
 
 int main()
 {
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
     const auto window = new BondEngine::Window(1300, 800, "BondEngine");
     const auto shader = new BondEngine::ShaderProgram("Assets/Shaders/Vertex3D.vert",
                                                       "Assets/Shaders/Fragment3D.frag");
     const auto cube = new BondEngine::Cube(shader);
+
+    VkInstance instance;
+    VkApplicationInfo appInfo{};
+    appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    appInfo.pApplicationName = "Vulkan App";
+    appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.pEngineName = "No Engine";
+    appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.apiVersion = VK_API_VERSION_1_0;
+
+    VkInstanceCreateInfo createInfo{};
+    createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    createInfo.pApplicationInfo = &appInfo;
+
+    if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS)
+    {
+        return -1;
+    }
 
     std::cout << (const char*)glGetString(GL_RENDERER);
 
@@ -25,6 +46,8 @@ int main()
         window->pollEvents();
         window->swapBuffers();
     }
+
+    vkDestroyInstance(instance, nullptr);
 
     return 0;
 }
